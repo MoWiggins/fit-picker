57e15979daf668aad1bd11b45cd782f60cc437bc
Mohamed Omar <omarmo1@yahoo.com>
2021-03-02 17:24:27 -0600

    added a next and previous button component

---
+++ src/components/nextbutton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styling/buttons.css';
+
+export default class NextButton extends React.Component{
+    render(){
+        return(
+            <a href="#" class="next round"> &#8250;</a>
+        )
+    }
+}
+
---
+++ src/components/previousbutton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styling/buttons.css';
+
+export default class PreviousButton extends React.Component{
+    render(){
+        return(
+            <a href="#" class="previous round">&#8249; </a>
+        )
+    }
+}
+
